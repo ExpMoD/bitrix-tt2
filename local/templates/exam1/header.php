0f6d2ba706b9b494b6655e655ca9513e0771fa0d
Andrey Kudakov <kudakovandrey@gmail.com>
2017-11-05 07:33:51 +0300

    Исправление 2

--- local/templates/exam1/header.php
+++ local/templates/exam1/header.php
@@ -34,7 +34,7 @@ $workTime = (date('G') >= 9) && (date('G') < 18);
     <!-- header -->
     <header class="header">
         <div class="inner-wrap">
-            <div class="logo-block"><a href="" class="logo">Мебельный магазин</a>
+            <div class="logo-block"><a href="/" class="logo">Мебельный магазин</a>
             </div>
             <div class="main-phone-block">
 	            <?if ($workTime):?>
